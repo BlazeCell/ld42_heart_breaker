aa8fb7a2aefa8494b08cb72e0a651a280130f64f
Brent Chance <blazecell@hotmail.com>
2018-08-12 01:32:45 -0700

    - Set build settings. - Added ESC to quit functionality.

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleManager : MonoBehaviour
+{
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			Debug.Log("Quitting...");
+			
+			Application.Quit();
+		}
+	}
+};
